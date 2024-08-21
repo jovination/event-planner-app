71d70674d38ba1d6c7837c33080f866af3cb58eb
jovination <jovination791@gmail.com>
2024-08-21 23:26:46 +0300

    working with bottom navgation

--- app/src/main/java/com/nanologic/eventify/Home.java
+++ app/src/main/java/com/nanologic/eventify/Home.java
@@ -1,5 +1,7 @@
 package com.nanologic.eventify;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,6 +9,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Home extends AppCompatActivity {
 
@@ -20,5 +24,8 @@ public class Home extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setItemActiveIndicatorColor(ColorStateList.valueOf(Color.TRANSPARENT));
+
     }
 }
