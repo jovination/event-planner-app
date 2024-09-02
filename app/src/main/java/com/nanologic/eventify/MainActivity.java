c059d47483035f7be41b7dc030039a4efe6d6cd4
jovination <jovination791@gmail.com>
2024-09-02 15:52:22 +0300

    few changes

--- app/src/main/java/com/nanologic/eventify/MainActivity.java
+++ app/src/main/java/com/nanologic/eventify/MainActivity.java
@@ -1,9 +1,13 @@
 package com.nanologic.eventify;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,21 +17,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Animation bottomAnim;
+    TextView logo;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
+        bottomAnim = AnimationUtils.loadAnimation(this , R.anim.bottom_animation);
+        logo = findViewById(R.id.textViewLogo);
+        logo.setAnimation(bottomAnim);
+
+        int SPLASH_TIMEOUT = 2000;
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent = new Intent(MainActivity.this, StartUp.class);
                 startActivity(intent);
                 finish();
-                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
-
             }
-        },2500);
+        }, SPLASH_TIMEOUT);
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
